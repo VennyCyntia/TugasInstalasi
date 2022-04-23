b0ff286c50e7019cab6bc090ab3e9b4ed0964bb7
unknown <venny.535200006@stu.untar.ac.id>
2022-04-23 16:19:50 +0700

    First-commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo'),
+          backgroundColor: Colors.teal,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Image(
+                image: AssetImage('images/picture.png'),
+              ),
+              Text('Hello World'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
